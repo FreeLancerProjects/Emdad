4900449ce180dd5d3ac7484e8a72954de4c6a228
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2019-05-21 15:20:51 +0200

    change

--- app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Fourth_shipping_Transpoortation.java
+++ app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Fourth_shipping_Transpoortation.java
@@ -1,7 +1,5 @@
 package com.creativeshare.emdad.Activites_Fragments.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,10 +11,10 @@ import android.view.ViewGroup;
 import com.creativeshare.emdad.R;
 
 
-public class Fragment_Third_shipping_Transpoortation extends Fragment {
+public class Fragment_Fourth_shipping_Transpoortation extends Fragment {
 
-    public static Fragment_Third_shipping_Transpoortation newInstance() {
-       return new Fragment_Third_shipping_Transpoortation();
+    public static Fragment_Fourth_shipping_Transpoortation newInstance() {
+       return new Fragment_Fourth_shipping_Transpoortation();
     }
 
 
@@ -24,7 +22,7 @@ public class Fragment_Third_shipping_Transpoortation extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view= inflater.inflate(R.layout.fragment_fragment__third_shipping__transpoortation, container, false);
+       View view= inflater.inflate(R.layout.fragment_fourth_shipping__transpoortation, container, false);
        return view;
     }
 
