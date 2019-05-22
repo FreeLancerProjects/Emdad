519bf0099d89d3962f7e346191e860cc3a2ff7c8
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2019-05-22 14:59:02 +0200

    change

--- app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Profile.java
+++ app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Profile.java
@@ -1,12 +1,17 @@
 package com.creativeshare.emdad.Activites_Fragments.Fragments;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import com.creativeshare.emdad.R;
 
@@ -15,7 +20,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Fragment_Profile extends Fragment {
     private CircleImageView circleImageView;
-
+    private Drawable progress;
+    private RatingBar ratingBar;
     public static Fragment_Profile newInstance() {
 
         return new Fragment_Profile();
@@ -34,6 +40,9 @@ public class Fragment_Profile extends Fragment {
     private void intitview(View view) {
         circleImageView=view.findViewById(R.id.accout_img);
         circleImageView.setImageResource(R.drawable.ic_photo_camera);
+        ratingBar=view.findViewById(R.id.myrating);
+        ratingBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.yellow1), PorterDuff.Mode.SRC_ATOP);
+       // DrawableCompat.setTint(progress, getResources().getColor(R.color.yellow1));
     }
 
 
