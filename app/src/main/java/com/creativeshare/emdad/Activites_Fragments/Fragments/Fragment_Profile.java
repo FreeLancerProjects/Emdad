502b6a2f5e49d71f840407dc61ed2650476b2dbd
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2019-05-23 11:28:05 +0200

    change

--- app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Profile.java
+++ app/src/main/java/com/creativeshare/emdad/Activites_Fragments/Fragments/Fragment_Profile.java
@@ -8,20 +8,28 @@ import android.os.Bundle;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.creativeshare.emdad.R;
+import com.google.android.material.appbar.AppBarLayout;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class Fragment_Profile extends Fragment {
     private CircleImageView circleImageView;
-    private Drawable progress;
+    private LinearLayout linearLayout;
     private RatingBar ratingBar;
+    AppBarLayout appBarLayout;
+
     public static Fragment_Profile newInstance() {
 
         return new Fragment_Profile();
@@ -38,11 +46,28 @@ public class Fragment_Profile extends Fragment {
     }
 
     private void intitview(View view) {
-        circleImageView=view.findViewById(R.id.accout_img);
+        circleImageView = view.findViewById(R.id.accout_img);
         circleImageView.setImageResource(R.drawable.ic_photo_camera);
-        ratingBar=view.findViewById(R.id.myrating);
+        linearLayout = view.findViewById(R.id.lin);
+        ratingBar = view.findViewById(R.id.myrating);
+        appBarLayout = view.findViewById(R.id.app_bar);
         ratingBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.yellow1), PorterDuff.Mode.SRC_ATOP);
-       // DrawableCompat.setTint(progress, getResources().getColor(R.color.yellow1));
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                //  Vertical offset == 0 indicates appBar is fully expanded.
+                if (Math.abs(verticalOffset) > 50) {
+
+                    circleImageView.setVisibility(View.GONE);
+                    linearLayout.setVisibility(View.GONE);
+                    ratingBar.setVisibility(View.GONE);
+                } else {
+                    circleImageView.setVisibility(View.VISIBLE);
+                    linearLayout.setVisibility(View.VISIBLE);
+                    ratingBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
 
