ec99dcdeef2db2f2be92e707b2ba17437ce4fdfb
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2019-05-23 14:39:52 +0200

    change

---
+++ app/src/main/java/com/creativeshare/emdad/Model/Contact_us_model.java
@@ -0,0 +1,25 @@
+package com.creativeshare.emdad.Model;
+
+public class Contact_us_model {
+
+    private String name;
+    private String email;
+    private String message;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- app/src/main/java/com/creativeshare/emdad/services/Services.java
+++ app/src/main/java/com/creativeshare/emdad/services/Services.java
@@ -1,8 +1,9 @@
 package com.creativeshare.emdad.services;
 
 
-
-
+import com.creativeshare.emdad.Model.Bank_Model;
+import com.creativeshare.emdad.Model.Contact_us_model;
+import com.creativeshare.emdad.Model.Information_Model;
 import com.creativeshare.emdad.Model.Slider_Model;
 import com.creativeshare.emdad.Model.UserModel;
 
@@ -36,8 +37,33 @@ public interface Services {
             @Field("phone") String phone,
             @Field("software_type") int software_type
     );
+
     @GET("api/slider")
     Call<Slider_Model> get_slider();
 
+    @GET("api/page/2")
+    Call<Information_Model>
+    gettermscondition();
 
+
+    @GET("api/page/1")
+    Call<Information_Model>
+    about_us();
+
+    @GET("api/accounts")
+    Call<Bank_Model> get_bank_accounts();
+
+    @FormUrlEncoded
+    @POST("api/callus")
+    Call<Contact_us_model> contact_us(
+            @Field("name") String name,
+            @Field("email") String email,
+            @Field("message") String message
+    );
+
+    @FormUrlEncoded
+    @POST("api/logout")
+    Call<ResponseBody> logout(
+            @Field("user_id") int user_id
+    );
 }
